ec3320b3bd5b400859063cabde16f978f7744570
Henrique Dalssaso <henrique@dalssaso.com.br>
2023-04-02 20:03:11 -0300

    add catppuccin-frappe as theme for neovim

--- dot_config/nvim/lua/dotfiles/packer.lua
+++ dot_config/nvim/lua/dotfiles/packer.lua
@@ -13,12 +13,18 @@ return require('packer').startup(function(use)
 	  requires = { {'nvim-lua/plenary.nvim'} }
   }
 
-  use({
-	  'rose-pine/neovim',
-	  as = 'rose-pine',
-	  config = function()
-		  vim.cmd('colorscheme rose-pine')
-	  end
+  --  use({
+--	  'rose-pine/neovim',
+--	  as = 'rose-pine',
+--	  config = function()
+--		  vim.cmd('colorscheme rose-pine')
+--	  end
+ -- })
+
+  use ({ "catppuccin/nvim", as = "catppuccin",
+    config = function()
+        vim.cmd('colorscheme catppuccin-frappe')
+    end
   })
 
   use({"nvim-treesitter/nvim-treesitter", run = ":TSUpdate"})
