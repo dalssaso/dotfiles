31eb90286d113d75ef60ca3642876ed13ca3f0eb
Henrique Dalssaso <henrique@dalssaso.com.br>
2023-04-03 15:13:45 -0300

    improve fish config

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -4,3 +4,13 @@ end
 
 starship init fish | source
 
+set -xg BAT_THEME "Catppuccin-frappe"
+set -xg EDITOR "nvim"
+alias vim='nvim'
+alias v='nvim'
+alias vi='nvim'
+
+alias ls='exa --git'
+alias cat='bat'
+
+set -xg LS_COLORS (vivid generate catppuccin-frappe)
