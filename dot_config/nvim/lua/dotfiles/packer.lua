1105183b71e877c481eeed534f1b816af81ff65d
Henrique Dalssaso <henrique@dalssaso.com.br>
2023-04-09 17:34:42 -0300

    Add comment plugin on nvim

--- dot_config/nvim/lua/dotfiles/packer.lua
+++ dot_config/nvim/lua/dotfiles/packer.lua
@@ -89,4 +89,11 @@ return require('packer').startup(function(use)
             end
         }
 
+        use {
+            'numToStr/Comment.nvim',
+            config = function()
+                require('Comment').setup()
+            end
+        }
+
     end)
