83e8d082f3896ad51f26ead8d2dad5377a1bc6cf
Henrique Dalssaso <henrique@dalssas.com.br>
2023-05-14 01:48:45 -0300

    removing everything

--- dot_config/nvim/lua/plugins/mason-null-ls.lua
+++
@@ -1,8 +0,0 @@
-return {
-    "jay-babu/mason-null-ls.nvim",
-    dependencies = {
-      "williamboman/mason.nvim",
-      "jose-elias-alvarez/null-ls.nvim",
-    },
-}
-
--- dot_config/nvim/lua/plugins/null_ls.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"jose-elias-alvarez/null-ls.nvim",
-	event = { "BufReadPre", "BufNewFile" },
-	config = function()
-		require("null-ls").setup()
-	end,
-	dependencies = { "nvim-lua/plenary.nvim" },
-}
