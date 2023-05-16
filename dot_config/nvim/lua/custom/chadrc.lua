a9ace1b9b40a05c24d39a66aa14eafaf9ba10297
Henrique Dalssaso <henrique@dalssaso.com.br>
2023-05-16 09:47:33 -0300

    change theme

--- dot_config/nvim/lua/custom/chadrc.lua
+++ dot_config/nvim/lua/custom/chadrc.lua
@@ -5,7 +5,7 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "onedark",
+  theme = "catppuccin",
   theme_toggle = { "onedark", "one_light" },
 
   hl_override = highlights.override,
