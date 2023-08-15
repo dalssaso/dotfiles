f1e7064b58adc9e947148bc7d83bff2126111d93
Henrique Dalssaso <henrique@dalssaso.com.br>
2023-08-15 12:58:34 -0300

    some more configuration

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,26 +1,22 @@
 if test -d /opt/homebrew/bin
-    set -p PATH $PATH /opt/homebrew/bin
+  set -p PATH /opt/homebrew/bin $PATH
 end
 
 if status is-interactive
-    zoxide init fish | source
+  zoxide init fish | source
 end
 
 # Old commands are not shown with full prompt
 function starship_transient_prompt_func
-    starship module character
+  starship module character
 end
 starship init fish | source
 enable_transience
 
-if test "$TERM" = xterm-kitty
-    alias ssh="kitty +kitten ssh"
-end
-
 if test -d $HOME/.bin
-    if not contains -- $HOME/.bin $PATH
-        set -p PATH $PATH $HOME/.bin
-    end
+  if not contains -- $HOME/.bin $PATH
+    set -p PATH $PATH $HOME/.bin
+  end
 end
 
 if test -d /opt/homebrew/opt/gnupg@2.2/bin
@@ -30,15 +26,45 @@ if test -d /opt/homebrew/opt/gnupg@2.2/bin
 end
 
 if test -d $HOME/.local/bin
-    if not contains -- $HOME/.local/bin $PATH
-        set -p PATH $PATH $HOME/.local/bin
-    end
+  if not contains -- $HOME/.local/bin $PATH
+    set -p PATH $PATH $HOME/.local/bin
+  end
 end
 
 if test -d $HOME/.cargo/bin
-    if not contains -- $HOME/.cargo/bin $PATH
-        set -p PATH $PATH $HOME/.cargo/bin
-    end
+  if not contains -- $HOME/.cargo/bin $PATH
+    set -p PATH $PATH $HOME/.cargo/bin
+  end
+end
+
+if test -d /opt/homebrew/opt/python@3.11/libexec/bin
+  if not contains -- /opt/homebrew/opt/python@3.11/libexec/bin $PATH
+    set -p PATH /opt/homebrew/opt/python@3.11/libexec/bin $PATH
+  end
+end
+
+if test -d /opt/homebrew/opt/grep/libexec/gnubin
+  if not contains -- /opt/homebrew/opt/grep/libexec/gnubin $PATH
+    set -p PATH /opt/homebrew/opt/grep/libexec/gnubin $PATH
+  end
+end
+
+if test -d /opt/homebrew/opt/binutils/bin
+  if not contains -- /opt/homebrew/opt/binutils/bin $PATH
+    set -p PATH /opt/homebrew/opt/binutils/bin $PATH
+  end
+end
+
+if test -d /opt/homebrew/opt/coreutils/libexec/gnubin
+  if not contains -- /opt/homebrew/opt/coreutils/libexec/gnubin $PATH
+    set -p PATH /opt/homebrew/opt/coreutils/libexec/gnubin $PATH
+  end
+end
+
+if test -d /opt/homebrew/opt/gnu-sed/libexec/gnubin
+  if not contains -- /opt/homebrew/opt/gnu-sed/libexec/gnubin $PATH
+    set -p PATH /opt/homebrew/opt/gnu-sed/libexec/gnubin $PATH
+  end
 end
 
 if test -d /opt/homebrew/opt/postgresql@15/bin
@@ -53,7 +79,7 @@ end
 set -gx PATH $PATH $HOME/.krew/bin
 
 if test -f $HOME/.asdf/asdf.fish
-    source $HOME/.asdf/asdf.fish
+  source $HOME/.asdf/asdf.fish
 end
 
 
@@ -70,13 +96,13 @@ set -xg LS_COLORS (vivid generate catppuccin-mocha)
 # --color=fg:#c6d0f5,header:#e78284,info:#ca9ee6,pointer:#f2d5cf \
 # --color=marker:#f2d5cf,fg+:#c6d0f5,prompt:#ca9ee6,hl+:#e78284"
 
-# tokyonight
-set -xg FZF_DEFAULT_OPTS "\
---color=fg:#c0caf5,bg:#1a1b26,hl:#ff9e64 \
---color=fg+:#c0caf5,bg+:#1a1b26,hl+:#ff9e64 \
---color=info:#7aa2f7,prompt:#7aa2f7,pointer:#db4b4b \
---color=marker:#9ece6a,spinner:#9ece6a,header:#9ece6a"
-
+# # tokyonight
+# set -xg FZF_DEFAULT_OPTS "\
+# --color=fg:#c0caf5,bg:#1a1b26,hl:#ff9e64 \
+# --color=fg+:#c0caf5,bg+:#1a1b26,hl+:#ff9e64 \
+# --color=info:#7aa2f7,prompt:#7aa2f7,pointer:#db4b4b \
+# --color=marker:#9ece6a,spinner:#9ece6a,header:#9ece6a"
+#
 # catppuccin-mocha
 set -Ux FZF_DEFAULT_OPTS "\
 --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
