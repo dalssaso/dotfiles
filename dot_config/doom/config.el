8ced72110c965c98984003f7093321434da9c3c0
Henrique Dalssaso <henrique@dalssaso.com.br>
2023-10-17 10:36:53 -0300

    add doom config

---
+++ dot_config/doom/config.el
@@ -0,0 +1,114 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets. It is optional.
+(setq user-full-name "Henrique Dalssaso"
+      user-mail-address "henrique@dalssaso.com.br")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom:
+;;
+;; - `doom-font' -- the primary font to use
+;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
+;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;; - `doom-unicode-font' -- for unicode glyphs
+;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
+;;
+;; See 'C-h v doom-font' for documentation and more examples of what they
+;; accept. For example:
+;;
+;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
+;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
+(setq doom-font (font-spec :family "Dank Mono" :size 17 :weight 'regular)
+      doom-variable-pitch-font (font-spec :family "Dank Mono" :size 17 :weight 'regular)
+      doom-unicode-font (font-spec :family "MesloLGS Nerd Font Mono" :size 17 :weight 'regular)
+      doom-big-font (font-spec :family "Dank Mono" :size 22 :weight 'regular))
+;;
+;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
+;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
+;; refresh your font settings. If Emacs still can't find your font, it likely
+;; wasn't installed correctly. Font issues are rarely Doom issues!
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-vibrant)
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/Dropbox/org/")
+
+
+;; Whenever you reconfigure a package, make sure to wrap your config in an
+;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
+;;
+;;   (after! PACKAGE
+;;     (setq x y))
+;;
+;; The exceptions to this rule:
+;;
+;;   - Setting file/directory variables (like `org-directory')
+;;   - Setting variables which explicitly tell you to set them before their
+;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
+;;   - Setting doom variables (which start with 'doom-' or '+').
+;;
+;; Here are some additional functions/macros that will help you configure Doom.
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
+;; etc).
+;;
+;; Forge stuff
+;;
+(setq forge-alist
+      '(("git.ifoodcorp.com.br" "code.ifoodcorp.com.br/api/v4" "code.ifoodcorp.com.br" forge-gitlab-repository)
+        ("github.com" "api.github.com" "github.com" forge-github-repository)
+        ("gitlab.com" "gitlab.com/api/v4" "gitlab.com" forge-gitlab-repository))
+      )
+
+(setq auth-sources '(macos-keychain-generic macos-keychain-internet "/Users/dalssaso/.config/emacs/.local/state/authinfo.gpg" "~/.authinfo.gpg" "~/.authinfo-work.gpg"))
+
+
+
+(setq projectile-project-search-path '("~/src/work/" "~/src/personal/" "~/.local/share/chezmoi"))
+(setq lsp-yaml-format-enable 'nil)
+
+;; Modes and start stuff
+(global-wakatime-mode)
+
+(setq nyan-animate-nyancat t)
+(setq nyan-wavy-trail t)
+(nyan-mode)
+;;
+(setq ob-mermaid-cli-path "/Users/dalssaso/.asdf/shims/mmdc")
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
+
+(require 'org-crypt)
+(require 'epa-file)
+(custom-set-variables
+  '(epg-gpg-program "/opt/homebrew/opt/gnupg@2.2/bin/gpg")
+)
+(epa-file-enable)
+(org-crypt-use-before-save-magic)
+(setq org-tags-exclude-from-inheritance (quote ("crypt")))
+(setq org-crypt-key nil)
