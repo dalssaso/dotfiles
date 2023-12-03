e013be763d9eaec213afcab296cfedd353e9d84b
Henrique Dalssaso <henrique@dalssaso.com.br>
2023-12-03 15:06:05 -0300

    change theme to nord

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -13,7 +13,7 @@ end
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = 'Ayu Mirage'
+config.color_scheme = 'nord'
 config.font = wezterm.font 'Iosevka Term'
 config.font_size = 17
 
