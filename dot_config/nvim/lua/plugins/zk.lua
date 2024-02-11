03a22d47de145503e1dcf76d4d45ce0aefaa8dd7
Henrique Dalssaso <henrique@dalssaso.com.br>
2024-02-11 00:13:13 -0300

    change everything to use lazyvim cus im lazy

---
+++ dot_config/nvim/lua/plugins/zk.lua
@@ -0,0 +1,29 @@
+return {
+  {
+    'zk-org/zk-nvim',
+    keys = {
+      { '<leader>zn', ':ZkNew<CR>', desc = 'New note', mode = 'n' },
+    },
+    config = function()
+      require('zk').setup({
+        picker = 'telescope',
+
+        lsp = {
+          config = {
+            cmd = { 'zk', 'lsp' },
+            name = 'zk',
+            on_attach = require('lazyvim.util.lsp').on_attach,
+          },
+
+          auto_attach = {
+            enabled = true,
+            filetypes = { 'markdown' },
+          },
+        },
+      })
+    end,
+  },
+  -- keys = {
+  --   { '<leader>zn', ':ZkNew<CR>', desc = 'New note' },
+  -- },
+}
