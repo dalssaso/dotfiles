8c8e625032925e85d9d2ab70b77a5f77c7c82780
Henrique Dalssaso <henrique@dalssaso.com.br>
2024-06-18 15:02:55 -0300

    add copilot chat

--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -14,6 +14,7 @@ require('lazy').setup({
     } },
     -- import any extras modules here
     { import = 'lazyvim.plugins.extras.coding.copilot' },
+    { import = 'lazyvim.plugins.extras.coding.copilot-chat' },
     { import = 'lazyvim.plugins.extras.dap.core' },
     { import = 'lazyvim.plugins.extras.editor.aerial' },
     { import = 'lazyvim.plugins.extras.editor.harpoon2' },
