1f876cbec24f9227dff51316c6aa0e445ecd75f6
Henrique Dalssaso <henrique@dalssaso.com.br>
2025-03-09 18:53:26 -0300

    feat(tools): change theme to rose-pine

--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -10,7 +10,7 @@ require('lazy').setup({
   spec = {
     -- add LazyVim and import its plugins
     { 'LazyVim/LazyVim', import = 'lazyvim.plugins', opts = {
-      colorscheme = 'catppuccin-mocha',
+      colorscheme = 'rose-pine-moon',
     } },
     -- import any extras modules here
     -- { import = 'lazyvim.plugins.extras.ai.copilot' },
@@ -55,7 +55,7 @@ require('lazy').setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  install = { colorscheme = { 'catppuccin', 'habamax' } },
+  install = { colorscheme = { 'rose-pine-moon', 'habamax' } },
   checker = { enabled = true }, -- automatically check for plugin updates
   performance = {
     rtp = {
---
+++ dot_config/private_fish/functions/k9s.fish
@@ -0,0 +1,5 @@
+function k9s_term
+    TERM="xterm-256color" /opt/homebrew/bin/k9s $argv
+end
+
+alias k9s k9s_term
