113ff296efa50a4725a86fb6269af51044bb2507
Henrique Dalssaso <henrique@dalssaso.com.br>
2025-08-10 20:30:45 -0300

    i dont even know at this time anymore

--- .chezmoiscripts/darwin/run_once_after_configure-ssh-keys.fish
+++ .chezmoiscripts/darwin/run_once_after_configure-ssh-keys.fish
@@ -5,5 +5,7 @@ op item get 'SSH - Work' --reveal --fields 'private key' | sed '1{/^"/d};$ {/^"/
 op item get 'SSH - Work' --fields 'public key' >~/.ssh/work.pub
 op item get 'SSH - Personal' --reveal --fields 'private key' | sed '1{/^"/d};$ {/^"/d}' >~/.ssh/id_rsa
 op item get 'SSH - Personal' --fields 'public key' >~/.ssh/id_rsa.pub
+op item get 'SSH - GetTheForkOff' --reveal --fields 'private key' | sed '1{/^"/d};$ {/^"/d}' >~/.ssh/oss
+op item get 'SSH - GetTheForkOff' --fields 'public key' >~/.ssh/oss.pub
 
-chmod 600 ~/.ssh/{work,id_rsa}
+chmod 600 ~/.ssh/{work,id_rsa,oss}
