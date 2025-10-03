a88486d1f1dac9963310115ffb00e0c3756e2826
Henrique Dalssaso <henrique@dalssaso.com.br>
2025-10-03 10:29:24 -0300

    change theme again

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -71,6 +71,10 @@ if test -d /opt/homebrew/opt/rustup/bin
     fish_add_path -Ua /opt/homebrew/opt/rustup/bin
 end
 
+if test -d $HOME/config/emacs/bin
+    fish_add_path -Ua $HOME/config/emacs/bin
+end
+
 varclear PATH
 
 #if test -f $HOME/.asdf/asdf.fish
@@ -83,18 +87,18 @@ set fish_greeting
 set -xg BAT_THEME_DARK Catppuccin-mocha
 set -xg BAT_THEME_LIGHT Catppuccin-latte
 set -xg EDITOR nvim
-set -xg LS_COLORS (vivid generate catppuccin-mocha)
+set -xg LS_COLORS (vivid generate catppuccin-latte)
 set -xg VISUAL nvim
 set -xg ZK_NOTEBOOK_DIR $HOME/Dropbox/zk
 set -xg GOPATH $HOME/go
 set -xg PIPX_HOME $HOME/.local/share/pipx
 
 set -Ux FZF_DEFAULT_OPTS "\
---color=bg+:#313244,bg:#1E1E2E,spinner:#F5E0DC,hl:#F38BA8 \
---color=fg:#CDD6F4,header:#F38BA8,info:#CBA6F7,pointer:#F5E0DC \
---color=marker:#B4BEFE,fg+:#CDD6F4,prompt:#CBA6F7,hl+:#F38BA8 \
---color=selected-bg:#45475A \
---color=border:#6C7086,label:#CDD6F4"
+--color=bg+:#CCD0DA,bg:#EFF1F5,spinner:#DC8A78,hl:#D20F39 \
+--color=fg:#4C4F69,header:#D20F39,info:#8839EF,pointer:#DC8A78 \
+--color=marker:#7287FD,fg+:#4C4F69,prompt:#8839EF,hl+:#D20F39 \
+--color=selected-bg:#BCC0CC \
+--color=border:#9CA0B0,label:#4C4F69"
 
 alias vim='nvim'
 alias v='nvim'
