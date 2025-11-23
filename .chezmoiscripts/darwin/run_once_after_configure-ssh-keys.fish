96d91e23600e69dae1594b5c734b83c489620457
Henrique Dalssaso <henrique@dalssaso.com.br>
2025-11-23 18:58:47 -0300

    other ton of changes

--- .chezmoiscripts/darwin/run_once_after_configure-ssh-keys.fish
+++ .chezmoiscripts/darwin/run_once_after_configure-ssh-keys.fish
@@ -7,5 +7,7 @@ op item get 'SSH - Personal' --reveal --fields 'private key' | sed '1{/^"/d};$ {
 op item get 'SSH - Personal' --fields 'public key' >~/.ssh/id_rsa.pub
 op item get 'SSH - GetTheForkOff' --reveal --fields 'private key' | sed '1{/^"/d};$ {/^"/d}' >~/.ssh/oss
 op item get 'SSH - GetTheForkOff' --fields 'public key' >~/.ssh/oss.pub
+op item get 'Chef - Work' --reveal --fields 'private key' | sed '1{/^"/d};$ {/^"/d}' >~/.chef/henrique_dalssaso.pem
 
 chmod 600 ~/.ssh/{work,id_rsa,oss}
+chmod 600 ~/.chef/henrique_dalssaso.pem
