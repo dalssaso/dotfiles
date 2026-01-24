82e4f55b6b8f2e0c53948ea73d39657fa2d23a17
Henrique Dalssaso <henrique@dalssaso.com.br>
2026-01-24 02:16:21 -0300

    fix

--- dot_config/nvim/lua/plugins/everforest.lua
+++ dot_config/nvim/lua/plugins/everforest.lua
@@ -3,12 +3,19 @@ return {
     "neanias/everforest-nvim",
     version = false,
     lazy = false,
-    priority = 1000,
+    priority = 1000, -- make sure to load this before all the other start plugins
+    -- Optional; default configuration will be used if setup isn't called.
     config = function()
       require("everforest").setup({
         background = "hard",
         italics = true,
       })
     end,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "everforest",
+    },
   },
 }
