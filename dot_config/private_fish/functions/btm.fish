fd4e295c0a9bb39fefb65068bf8dc9614d31a396
Henrique Dalssaso <henrique@dalssaso.com.br>
2026-08-28 11:17:22 -0300

    feat(bottom): generate dragon and lotus configs with fish wrapper

---
+++ dot_config/private_fish/functions/btm.fish
@@ -0,0 +1,7 @@
+function btm --wraps btm --description 'btm with the current appearance theme'
+    set -l cfg $__theme_btm_config
+    if test -z "$cfg"; or not test -r "$cfg"
+        set cfg ~/.config/bottom/bottom-dragon.toml
+    end
+    command btm -C $cfg $argv
+end
