10e1844c09b330ad0afcf63feaf6313e28d5e436
Henrique Dalssaso <henrique@dalssaso.com.br>
2026-09-17 07:10:40 -0300

    feat(nvim): replace neovim-ayu with kanagawa wave/lotus

---
+++ dot_config/nvim/lua/plugins/kanagawa.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "rebelot/kanagawa.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("kanagawa").setup({
+        compile = false,
+        theme = "wave",
+        background = { dark = "wave", light = "lotus" },
+      })
+    end,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "kanagawa",
+    },
+  },
+}
